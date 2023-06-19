3df46769d17dccd002658f9560dad9d48e858567
Alexandre <alexandrefaria3012@gmail.com>
2023-06-19 17:43:13 -0300

    Update calculadoraImc.dart

--- lib/pages/calculadoraImc.dart
+++ lib/pages/calculadoraImc.dart
@@ -23,47 +23,59 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const Icon(
-            Icons.person_outline,
-            size: 120.0,
-            color: (Colors.green),
-          ),
-          const TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Peso (kg)',
-              labelStyle: TextStyle(color: Colors.green),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Icon(
+              Icons.person_outline,
+              size: 120.0,
+              color: (Colors.green),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green),
-          ),
-          const TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Altura (cm)',
-              labelStyle: TextStyle(color: Colors.green),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Peso (kg)',
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              primary: const Color(0xff00d7f3),
-              //padding: const EdgeInsets.all(16),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Altura (cm)',
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green),
             ),
-            child: const Text(
-              'Calcular',
+            const SizedBox(width: 8),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: const Color(0xff4caf50),
+                padding: const EdgeInsets.only(top:10.0, bottom: 10.0),
+              ),
+              child: const Text(
+                'Calcular',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+            const Text(
+              "Info",
+              textAlign: TextAlign.center,
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.green,
                 fontSize: 25.0,
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
